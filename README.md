041ffcdce221d96e265ef46d1013d8826e10999f
dev-gabs <45233829+GJSdev@users.noreply.github.com>
2019-12-04 12:51:43 -0300

    Projeto inicial de eventos.
    Signed-off-by: dev-gabs <45233829+GJSdev@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,1 @@
+## proAgil Eventos
---
+++ proAgil.api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using proAgil.api.Models;
+using proAgil.api.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+
+namespace proAgil.api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        public readonly DataContext _context;
+        public WeatherForecastController(DataContext context)
+        {
+            _context = context;
+        }
+                
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var results = await _context.Eventos.ToListAsync();
+                return Ok(results);
+            }
+            catch (System.Exception)
+            {
+                
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de Dados Falhou");
+            }            
+            
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetId(int id)
+        {
+            try
+            {
+                var results = await _context.Eventos.FirstOrDefaultAsync(x=>x.EventoId == id);
+                return Ok(results);
+            }
+            catch (System.Exception)
+            {
+                
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de Dados Falhou");
+            }
+            
+        }
+    } 
+}
